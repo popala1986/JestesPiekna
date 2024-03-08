8f887b90fbbb30ec9101135554c227ccbec43974
Paweł Opala <p.opala1986@gmail.com>
2024-03-08 21:07:05 +0100

    Initial commit

---
+++ src/test/java/pl/JestesPiekna/JpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.JestesPiekna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
