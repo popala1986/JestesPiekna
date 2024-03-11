586edc5669563db0f2685c1b764f8ff1eebbb7af
Paweł Opala <p.opala1986@gmail.com>
2024-03-11 15:41:39 +0100

    the findUserByActivationToken method was added to UserRepository.

--- src/main/java/pl/JestesPiekna/registration/repository/UserRepository.java
+++ src/main/java/pl/JestesPiekna/registration/repository/UserRepository.java
@@ -2,6 +2,8 @@ package pl.JestesPiekna.registration.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.JestesPiekna.model.User;
 
 
@@ -13,5 +15,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 
     User findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.activationToken = :activation_token")
+    User findUserByActivationToken(@Param("activation_token") String activation_token);
 
 }
