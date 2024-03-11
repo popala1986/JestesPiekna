6604f902fc92fae50448457eb88a3d77c49d3b90
Paweł Opala <p.opala1986@gmail.com>
2024-03-11 14:00:11 +0100

    - edit UserRepository.

--- src/main/java/pl/JestesPiekna/registration/repository/UserRepository.java
+++ src/main/java/pl/JestesPiekna/registration/repository/UserRepository.java
@@ -2,7 +2,7 @@ package pl.JestesPiekna.registration.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
+import pl.JestesPiekna.model.User;
 
 
 import java.util.Optional;
