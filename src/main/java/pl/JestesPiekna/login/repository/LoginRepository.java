669aa662df2386056abb28683d1d1ce5623c74c6
Paweł Opala <p.opala1986@gmail.com>
2024-03-12 09:08:20 +0100

    A new findByUsername method has been introduced in the LoginRepository interface, allowing you to find a user based on the username.

--- src/main/java/pl/JestesPiekna/login/repository/LoginRepository.java
+++ src/main/java/pl/JestesPiekna/login/repository/LoginRepository.java
@@ -4,7 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.JestesPiekna.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface LoginRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByUsername(String username);
+
 
 }
