8c719479f9acce2589059509974ce16bb557b3ee
Paweł Opala <p.opala1986@gmail.com>
2024-03-14 16:39:43 +0100

    HomePageController has been edited.

--- src/main/java/pl/JestesPiekna/homePage/controller/HomePageController.java
+++ src/main/java/pl/JestesPiekna/homePage/controller/HomePageController.java
@@ -1,14 +1,27 @@
 package pl.JestesPiekna.homePage.controller;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.JestesPiekna.model.PhotoGallery;
+import pl.JestesPiekna.photoGallery.service.PhotoGalleryService;
+
+import java.util.List;
 
 
 @Controller
 public class HomePageController {
 
+    private final PhotoGalleryService photoGalleryService;
+
+    public HomePageController(PhotoGalleryService photoGalleryService) {
+        this.photoGalleryService = photoGalleryService;
+    }
+
+
     @GetMapping("/YouAreBeautiful")
-    public String showHomePage() {
+    public String showHomePage(Model model) {
+        List<PhotoGallery> allPhotos = photoGalleryService.getAllPhotos();
+        model.addAttribute("allPhotos", allPhotos);
         return "homePageAll";
     }
 
