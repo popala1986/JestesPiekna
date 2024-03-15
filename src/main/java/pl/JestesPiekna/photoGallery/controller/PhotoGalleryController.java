e5ca1d58fc24a11d567d820eab134fe694e40814
Paweł Opala <p.opala1986@gmail.com>
2024-03-15 08:26:35 +0100

    Edit PhotoController and PhotoGalleryController.

---
+++ src/main/java/pl/JestesPiekna/photoGallery/controller/PhotoGalleryController.java
@@ -0,0 +1,33 @@
+package pl.JestesPiekna.photoGallery.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.JestesPiekna.model.PhotoGallery;
+import pl.JestesPiekna.photoGallery.service.PhotoGalleryService;
+
+import java.util.List;
+
+@Controller
+public class PhotoGalleryController {
+
+    private final PhotoGalleryService photoGalleryService;
+
+    public PhotoGalleryController(PhotoGalleryService photoGalleryService) {
+        this.photoGalleryService = photoGalleryService;
+    }
+
+    @GetMapping("/galleryAll")
+    public String showGalleryAll(Model model) {
+        List<PhotoGallery> AllPhotos  = photoGalleryService.getAllPhotos();
+        model.addAttribute("AllPhotos", AllPhotos);
+        return "galleryPageAll";
+    }
+
+    @GetMapping("/gallery")
+    public String showGallery(Model model) {
+        List<PhotoGallery> AllPhotos  = photoGalleryService.getAllPhotos();
+        model.addAttribute("AllPhotos", AllPhotos);
+        return "galleryPage";
+    }
+}
