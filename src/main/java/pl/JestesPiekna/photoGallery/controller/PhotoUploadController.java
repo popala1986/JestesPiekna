5de32c448fbfd8b807f3a05a793728a9de3523d3
Paweł Opala <p.opala1986@gmail.com>
2024-03-20 13:11:19 +0100

    @PreAuthorize("hasRole('ROLE_ADMIN')") annotations has been added to PhotoUploadController.

--- src/main/java/pl/JestesPiekna/photoGallery/controller/PhotoUploadController.java
+++ src/main/java/pl/JestesPiekna/photoGallery/controller/PhotoUploadController.java
@@ -1,6 +1,7 @@
 package pl.JestesPiekna.photoGallery.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +25,7 @@ public class PhotoUploadController {
         this.photoGalleryService = photoGalleryService;
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/uploadForm")
     public String showUploadForm() {
         return "uploadPhoto";
