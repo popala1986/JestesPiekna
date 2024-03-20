2f998216e20fa5702c7eaa0f259104698b86e293
Paweł Opala <p.opala1986@gmail.com>
2024-03-20 13:10:41 +0100

    @EnableMethodSecurity(securedEnabled = true) annotations has been added to SecurityConfig.

--- src/main/java/pl/JestesPiekna/configuration/SecurityConfig.java
+++ src/main/java/pl/JestesPiekna/configuration/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -34,6 +35,7 @@ import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 @Profile("prod")
 public class SecurityConfig {
 
