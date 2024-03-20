d0e8a2f6d37aa1cffd8869cecad7e3d50ea82c98
Paweł Opala <p.opala1986@gmail.com>
2024-03-20 11:56:53 +0100

    new LogoutController class has been created;

---
+++ src/main/java/pl/JestesPiekna/logout/LogoutController.java
@@ -0,0 +1,14 @@
+package pl.JestesPiekna.logout;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class LogoutController {
+
+    @GetMapping("/logout")
+    public String logout() {
+        return "redirect:/YouAreBeautiful";
+    }
+}
