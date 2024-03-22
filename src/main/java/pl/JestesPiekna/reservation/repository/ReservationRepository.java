5dd13eed1e8d764da7d1db0274287fba09bc684a
Paweł Opala <p.opala1986@gmail.com>
2024-03-22 11:02:58 +0100

    Added new method :  findAllByClient responsible for return client(User).

---
+++ src/main/java/pl/JestesPiekna/reservation/repository/ReservationRepository.java
@@ -0,0 +1,25 @@
+package pl.JestesPiekna.reservation.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pl.JestesPiekna.model.Reservation;
+import pl.JestesPiekna.model.ServiceType;
+import pl.JestesPiekna.model.User;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
+
+
+    @Query("SELECT COUNT(r) > 0 FROM Reservation r WHERE r.reservation_date = :reservationDate AND r.service_type = :serviceType")
+    boolean existsByReservationDateAndServiceType(@Param("reservationDate") Date reservationDate, @Param("serviceType") ServiceType serviceType);
+
+    List<Reservation> findAllByClient(User client);
+
+
+}
