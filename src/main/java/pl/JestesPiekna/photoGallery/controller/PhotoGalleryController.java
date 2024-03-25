ef6f9cffa4ebb40ca62930fb6789e5f39a273138
Paweł Opala <p.opala1986@gmail.com>
2024-03-25 13:17:58 +0100

    added annotations.

--- src/main/java/pl/JestesPiekna/photoGallery/controller/PhotoGalleryController.java
+++ src/main/java/pl/JestesPiekna/photoGallery/controller/PhotoGalleryController.java
@@ -1,11 +1,11 @@
 package pl.JestesPiekna.photoGallery.controller;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import pl.JestesPiekna.model.PhotoGallery;
 import pl.JestesPiekna.photoGallery.service.PhotoGalleryService;
+import pl.JestesPiekna.reservation.service.ReservationService;
 
 import java.util.List;
 
@@ -14,8 +14,11 @@ public class PhotoGalleryController {
 
     private final PhotoGalleryService photoGalleryService;
 
-    public PhotoGalleryController(PhotoGalleryService photoGalleryService) {
+    private final ReservationService reservationService;
+
+    public PhotoGalleryController(PhotoGalleryService photoGalleryService, ReservationService reservationService) {
         this.photoGalleryService = photoGalleryService;
+        this.reservationService = reservationService;
     }
 
     @GetMapping("/gallery")
